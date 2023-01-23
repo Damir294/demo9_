b85ce6974426f0281d84b27ab48447209e0bf6f2
Damir294 <117120261+Damir294@users.noreply.github.com>
2023-01-23 23:56:08 +0400

    Дебаг

---
+++ services/FileService.java
@@ -0,0 +1,21 @@
+package services;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public interface FileService {
+
+
+    void saveToJsonFile(Object object, String fileName);
+
+    String readFromFile(String fileName);
+
+
+    void uploadFile(MultipartFile file, String filename) throws IOException;
+
+    Path CreateTempFile(String suffix);
+}
